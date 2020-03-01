da2bf43bf12484e546572c1eed96bbcc8eaf03ed
Leevon Levasseur <leevonlevasseur@gmail.com>
2020-02-29 20:23:13 -0800

    test

--- a2/part3/diningOut.c
+++ a2/part3/diningOut.c
@@ -24,8 +24,7 @@ void check_for_forks(int current_id)
         first_fork = 0;
         second_fork = N - 1;
     }
-    if((strcmp(state[current_id], "Hungry") == 0)
-    && (strcmp(state[first_fork], "Eating") != 0) && (strcmp(state[second_fork], "Eating") != 0))
+    if((strcmp(state[current_id], "Hungry") == 0) && (strcmp(state[first_fork], "Eating") != 0) && (strcmp(state[second_fork], "Eating") != 0))
     {
         printf("hi\n");
         state[current_id] = "Eating";
